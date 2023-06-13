6cb38e1bb9e91511dc73c2365945ee4a64f4fcb7
Julien Merion <julien.merion@whitespace.co.uk>
2023-06-13 23:28:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -56,8 +56,7 @@ function simplifyDebts(transactions){
         if(minMax[0] == undefined || minMax[1] == undefined) return;
         let min_value = Math.min(-transaction_map.get(minMax[0]), transaction_map.get(minMax[1]));
 
-        const isZero = min_value === 0;
-        if(!isZero) {
+        if(min_value !== 0) {
             transaction_map.set(minMax[0], transaction_map.get(minMax[0]) + min_value);
             transaction_map.set(minMax[1], transaction_map.get(minMax[1]) - min_value);
 
